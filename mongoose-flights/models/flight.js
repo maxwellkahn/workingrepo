495f3cd0ae8036b5b9aa1119b02f1ee762b16586
maxwelljkahn <maxwelljkahn@gmail.com>
2021-07-01 16:24:24 -0700

    adds flight models

---
+++ mongoose-flights/models/flight.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const flightSchema = new Schema({
+    arline: {
+        type: String,
+        enum: ['American', 'Southwest', 'United']
+    },
+    airport: {
+        type: String,
+        enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN']
+        default: 'DEN'
+    },
+    flightNo: {
+        type: Number,
+        min: 10,
+        max: 9999
+    }
+    departs: {
+        type: Date,
+        default: function() {
+            return new Date().getFullYear() +1;
+        },
+    }
+})
